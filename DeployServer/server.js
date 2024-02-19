df891d459440806a876e41db505af3ec140bc868
BlueThinks <bluekitt4u@gmail.com>
2024-02-19 14:31:58 +0200

    Added commands command

--- DeployServer/server.js
+++ DeployServer/server.js
@@ -1,9 +1,12 @@
 const http = require('node:http'); //http module
 const fs = require('fs'); //filesystem
+const path = require('path');
 
 const config = require('./config.json'); //custom configurations file for secret info
 const { measureMemory } = require('node:vm');
 const { text } = require('stream/consumers');
+const { errorMonitor } = require('node:events');
+const { stringify } = require('node:querystring');
 const serverCommands = fs.readdirSync('./commands'); //folder to create commands in
 
 const hostname = config.hostname;
@@ -16,47 +19,78 @@ const isFile = fileName => {
     return fs.lstatSync(fileName).isFile();
 };
 
+function readFiles(dir, processfile) {
+    fs.readdir(dir, (error, fileNames) => {
+        if (error) throw error;
+
+        fileNames.forEach(filename => {
+            const name = path.parse(filename).name;
+            const ext = path.parse(filename).ext;
+            const filepath = path.resolve(dir, filename);
+
+            fs.stat(filepath, function(error, stat) {
+                if (error) throw error;
+                const isFile = stat.isFile();
+                
+                if (isFile) {
+                    processfile(filepath, name, ext, stat);
+                }
+            });
+        });
+    });
+};
+
 const server = http.createServer()
-server.on('request', async(req, res) => {
+server.on('request', async(request, response, callback) => {
     
-    message = req.url;
+    message = request.url;
     msg = message.slice(1); //cutting the / out of message
     
 
 
 
     
-    res.statusCode = 200;
-    res.setHeader('Content-type', 'text/plain');
+    response.statusCode = 200;
+    response.setHeader('Content-type', 'text/plain');
+
+    if (msg == 'commands') {
+        var files = fs.readdirSync('./commands/');
+        let original = files
+        result = original.map(function(d) {
+            return d.replace('.js', '');
+        });
+        stringCMD = result.toString();
+        response.end(stringCMD);
+    }
 
     //restart on command
     if (msg == 'restart') {
         console.log('Restarting the server...')
+        response.end('Restarting...\n');
         setTimeout(function() {
-            res.end('Restarting...\n');
             console.log('Restarting')
             process.exit(128)
-        }, 1500)
+        }, 2000);
+        
     }
-
-    
 
     //shutdown on command
     if (msg == 'shutdown') {
         console.log('Shutting down the server...')
         setTimeout(function() {
-            res.end('Shutting down...\n');
+            response.end('Shutting down...\n');
             console.log('Shutting down.')
             process.exit(0)
         }, 1500)
     }
 
-    else{ //if nothing matches
-        res.statuscode = 204;
-        res.end('Hello World\n');
+    else { //if nothing matches
+        response.statuscode = 204;
         console.log(msg);
+        response.end("");
         //204 no content available
     }
+    return;
 });
 
 server.listen(port, hostname, () => {
