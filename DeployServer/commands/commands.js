c96ecf1ca5cbd8e03024bbbbdba64af8f854e1ca
BlueThinks <bluekitt4u@gmail.com>
2024-09-23 13:41:43 +0300

    made and fixed commands command

--- DeployServer/commands/commands.js
+++ DeployServer/commands/commands.js
@@ -5,6 +5,12 @@ module.exports = {
 }
 function commands(){ //function to get current depositories and create array out of them
     //need to get every file in commands folder
-    data = "yo"
+    var files = fs.readdirSync('./commands/');
+    let original = files;
+    fs.close;
+    commands = original.map(function(d) {
+        return d.replace('.js', "");
+    });
+    var data = commands.toString();
     return data
 }
