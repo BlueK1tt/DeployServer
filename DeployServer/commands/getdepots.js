427325c0d614187418a8bd3e2667f01e0253a384
BlueThinks <bluekitt4u@gmail.com>
2024-09-24 15:33:21 +0300

    Added comment and kinda start for searching for correct object from array

--- DeployServer/commands/getdepots.js
+++ DeployServer/commands/getdepots.js
@@ -3,15 +3,21 @@ const fs = require('fs'); //filesystem
 module.exports = {
     data: getdepots()
 }
-function getdepots(){ //function to get current depositories and create array out of them
+function getdepots(msg){ //function to get current depositories and create array out of them
     //const data = fs.readFile("./Depositories/DepositoriesList.json");
     let rawdata = fs.readFileSync('./Depositories/DepositoriesList.json')
     let json = JSON.parse(rawdata);
+    console.log("msg: " + msg);
         if (json == " "){
             console.log("no current depositories")
         }
         else {
-            json = json.depositories.Jorma;
+            //need to make json output into array, and with msg specification can search correct object
+            if(msg = ""){
+
+            }
+            if(msg = somevariable)
+            json = json.depositories;
             let depots = JSON.stringify(json);
             return depots;
         }
