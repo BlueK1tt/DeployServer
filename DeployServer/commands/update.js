12000174e71a0619e3e24a97c95bbc8cd7316e0b
BlueThinks <bluekitt4u@gmail.com>
2024-10-19 15:32:29 +0300

    slice update message

--- DeployServer/commands/update.js
+++ DeployServer/commands/update.js
@@ -8,7 +8,6 @@ module.exports = {
 
 function update() {
     command = JSON.stringify(msg);
-    console.log("update:" + command)
     var files = fs.readdirSync('./Depositories/');
     let original = files
     result = original.map(function(d) {
@@ -17,14 +16,19 @@ function update() {
     result.pop(); //removes last object
     if (result == '') {
         data = "You haven't added any programs"
+    }
+    if(command.includes("update=")){
+        filter = command.slice(8)
+        return filter
+    
     } else { 
         send = result.toString();
-        data = "Repositories:"+ send + command; 
+        data = "Repositories:"+ send; 
     } 
     fs.close;
     return data;
 }
 
-function verifyfile(){
-
+function verifyfile(filter){
+    
 }
