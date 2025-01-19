654c694e8c14e0e3f8333fe6286fe69b784b06cc
BlueThinks <bluekitt4u@gmail.com>
2025-01-19 22:36:31 +0200

    Just need to add the actual downloading of repository from github

--- DeployServer/functions/install.js
+++ DeployServer/functions/install.js
@@ -1,4 +1,5 @@
 const fs = require('fs'); //filesystem
+const config = require("../resources/config.json");
 
 //install "program" to depositories folder
 //need to connect to github and download same named repository from there
@@ -34,12 +35,41 @@ function install(){
 
             //need download the repository into the folder
 
+
+
             //need to verify theres starting file, same way as *findfile
-            return "installing..."
+            instaldone = verifyfiles(filename);
+            if(instaldone == true){
+                console.log("instllation completed")
+                return "installing..."
+            }
+            else {
+                console.log("Uknown error")
+            }
         }
 
     } else {
         console.log("error");
         return "error;"
+    }
+}
+
+
+function verifyfiles(filename){
+    let files = fs.readdirSync(`./depositories/`+ `${filename}`);
+    fs.close
+
+    mainfiles = config.mainfiles;
+    var examplefiles = mainfiles.split(",");
+                
+    str1 = files.toString();
+
+    const filexist = examplefiles.filter(element => str1.includes(element))
+    if (filexist != null){
+
+        return true //true/false if example file exists in depository
+    }
+    else{
+        return false;
     }
 }
