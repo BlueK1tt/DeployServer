52270d018a1de21396e5a178c8ff260753a8a1b3
BlueThinks <bluekitt4u@gmail.com>
2025-01-28 22:27:52 +0200

    Created quick and simple webapp just for testing pm2 start/stop

---
+++ DeployServer/Depositories/Ticker/main.js
@@ -0,0 +1,17 @@
+http = require('node:http');
+app = function (request, response) {
+   // Send the HTTP header 
+   // HTTP Status: 200 : OK
+   // Content Type: text/html
+   response.writeHead(200, {'Content-Type': 'text/html'});
+  
+   // Send the response body as "Hello World"
+   response.end('<h2 style="text-align: center;">Hello World</h2>');
+};
+
+server = http.createServer(app);
+server.listen(3000);
+
+// Console will print the message
+
+console.log('App running at http://127.0.0.1:3001/');
