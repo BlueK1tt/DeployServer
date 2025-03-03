592693c2fda22dbbb3942f8f488c3879f035f6af
BlueThinks <bluekitt4u@gmail.com>
2025-03-03 02:19:42 +0200

    Some testing with html code from js server, need to do bare bones or figure out way to route css directly to js

--- DeployServer/Depositories/Ticker/main.js
+++ DeployServer/Depositories/Ticker/main.js
@@ -1,32 +1,34 @@
 http = require('node:http');
 pm2 = require('pm2');
+fs = require('fs');
 var path = require('path')
 
 var filename = path.basename(__dirname);
 
-app = function (request, response) {
-   // Send the HTTP header 
-   // HTTP Status: 200 : OK
-   // Content Type: text/html
-   response.writeHead(200, {'Content-Type': 'text/html'});
-  
-   // Send the response body as "Hello World"
-   response.end('<h2 style="text-align: center;">Hello World</h2>');
+function sendtomaster(data){
+  process.send({ //this is just example, boiletplate for future apps
+    type : 'process:msg',
+    data : {
+      app : data,
+      msg : "Hello World!"
+    }
+  })
+}
 
-   
-};
+app = fs.readFile('./Depositories/Ticker/index.html', function (err, html) {
+  if (err) {
+      throw err; 
+  }       
+  http.createServer(function(request, response) {  
+      response.writeHeader(200, {"Content-Type": "text/html"});  
+      response.write(html);  
+      response.end();
+  }).listen(3001);
+});
 
-server = http.createServer(app);
-server.listen(3000);
 
 
-process.send({ //this is just example, boiletplate for future apps
-   type : 'process:msg',
-   data : {
-     app : filename,
-     msg : "Hello World!"
-   }
- })
+
  
 // Console will print the message
 
