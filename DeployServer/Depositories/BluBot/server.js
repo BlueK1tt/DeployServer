4bc1086b4eac06a20611fe58f78c91f604e3928a
BlueThinks <bluekitt4u@gmail.com>
2025-03-05 01:20:45 +0200

    just some testing with discord bot | status:broken

--- DeployServer/Depositories/BluBot/server.js
+++ DeployServer/Depositories/BluBot/server.js
@@ -1,12 +1,33 @@
 const Discord = require('discord.js');
 const config = require('./config.json');
 var path = require('path');
-const { error } = require('console');
-const bot = new Discord.Client({intents: [Discord.GatewayIntentBits.Guilds],});
+const fs = require('fs'); //filesystem
+const { Client, Collection, Events, GatewayIntentBits } = require('discord.js');
+const bot = new Client({ intents: [GatewayIntentBits.Guilds] });
+
 
 const PREFIX = config.prefix
 var filename = path.basename(__dirname);
 
+bot.commands = new Collection();
+
+const foldersPath = path.join(__dirname, 'commands');
+const commandFolders = fs.readdirSync(foldersPath);
+
+for (const folder of commandFolders) {
+	const commandsPath = path.join(foldersPath, folder);
+	const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+	for (const file of commandFiles) {
+		const filePath = path.join(commandsPath, file);
+		const command = require(filePath);
+		// Set a new item in the Collection with the key as the command name and the value as the exported module
+		if ('data' in command && 'execute' in command) {
+			bot.commands.set(command.data.name, command);
+		} else {
+			console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+		}
+	}
+}
 
 /*function sendtomaster(data){
     process.send({ //this is just example, boiletplate for future apps
@@ -18,9 +39,15 @@ var filename = path.basename(__dirname);
     })
 }
 */
+
+bot.on(Events.InteractionCreate, interaction => {
+	console.log(interaction);
+});
+
 bot.on('ready', () =>{
     console.log(config.botname, 'Bot online'); //after online, post when last online, with info of how long was online, coudl store data in txt file
     bot.channels.cache.get(config.channel).send("`YO`");
+
 });
 
 bot.on('error', error => {
@@ -32,25 +59,33 @@ bot.on('uncaughtException', err => {
     process.exit(1) //mandatory (as per the Node.js docs)
 })
 
+bot.on(Events.InteractionCreate, async interaction => {
+	if (!interaction.isChatInputCommand()) return;
+
+	const command = interaction.bot.commands.get(interaction.commandName);
+
+	if (!command) {
+		console.error(`No command matching ${interaction.commandName} was found.`);
+		return;
+	}
+
+	try {
+		await command.execute(interaction);
+	} catch (error) {
+		console.error(error);
+		if (interaction.replied || interaction.deferred) {
+			await interaction.followUp({ content: 'There was an error while executing this command!', flags: MessageFlags.Ephemeral });
+		} else {
+			await interaction.reply({ content: 'There was an error while executing this command!', flags: MessageFlags.Ephemeral });
+		}
+	}
+});
 
 bot.on('message', message=>{
 	
     console.log(message.content)
+    console.log(message)
 
-    if (!message.content.startsWith(PREFIX) || message.author.bot) return;
-	
-	const args = message.content.slice(PREFIX.length).trim().split(/ +/);
-	const command = args.shift().toLowerCase();
-
-    if (!bot.commands.has(command)) return;
-	
-	try {
-		bot.commands.get(command).callback(message, args);
-	} catch (error) {
-		console.error(error);
-		message.reply('there was an error trying to execute that command!');
-	}
 });
-
 bot.login(config.token);
 //sendtomaster("BlutBot online");
