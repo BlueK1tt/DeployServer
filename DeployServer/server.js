3ba266d8d3c1612ec6e053f3ca565df55628c811
BlueThinks <bluekitt4u@gmail.com>
2025-03-13 13:22:28 +0200

    stuff

--- DeployServer/server.js
+++ DeployServer/server.js
@@ -131,7 +131,7 @@ function msgidentify(msg){ //c = different incoming msg
     console.log("id:" + msgid);
 
     if(msg == ""){
-        return "no specified command";
+        return "no specified command";kkkkkkkkkkkkkkkkkkkkkkkkkkkkk
     }
     if(basecommands.includes(msg)){
         console.log("base command");
