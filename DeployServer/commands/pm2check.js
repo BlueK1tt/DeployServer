1f3eecf2ba2d0c9eb1d1cc3dca0ce5e9578b7a26
BlueThinks <bluekitt4u@gmail.com>
2025-03-19 14:21:52 +0200

    testing for pm2check and sending data to main

--- DeployServer/commands/pm2check.js
+++ DeployServer/commands/pm2check.js
@@ -1,5 +1,12 @@
 const pm2 = require('pm2');
+const { send } = require('process');
+fs = require('fs');
+var path = require('path');
+var filename = path.basename(__dirname);
 
+
+const sendArray = [];
+sendtomain = [];
 
 module.exports = {
     data: pm2check()
@@ -7,52 +14,33 @@ module.exports = {
 
 function pm2check(){
     //console.log("pm2check");
-    try{
-        pm2.list((err, list) => {
+    pm2.list((err, list) => {
         const id = 0;
+        list = list.map(item => {
+            return item.id !== id ? item : null;
+        }).filter(item => item !== null)
         
-            list = list.map(item => {
-                return item.id !== id ? item : null;
-            }).filter(item => item !== null)
-            
-            servcount = Object.keys(list).length
-            //need to test out if code goes procedurally if take the "if" statements away
-            //they just served as check for other command, but now they arent needed, but they functionality is useful
-
-            
-            if(servcount >= 1){ //if no error and list not empty
-                //need to stop all running daemons
-                list.forEach((Element) => {
-                    //console.log(Element)
-                    return;
-                })
-            if(err != null){
-                console.log(err)
-                return err
-            }
-            else {
-                list.forEach((Element) => {
-                    filename = Element.name; //main.js server.js
-                    filetree = Element.pm2_env.pm_exec_path // /DeployServer/Depositories/
-                    pm2stat = Element.pm2_env.status //online , offline, stopped
-                    result = makepretty(filename, filetree, pm2stat);
-                    console.log(result);
-                    return result;
-            });
-                return "ok"
-            };
+        servcount = Object.keys(list).length
+        //need to test out if code goes procedurally if take the "if" statements away
+        //they just served as check for other command, but now they arent needed, but they functionality is useful
+        if(err != null){
+            console.log(err)
         }
-        return;
+        else {
+            list.forEach((Element) => {
+                filetree = Element.pm2_env.pm_exec_path // /DeployServer/Depositories/
+                pm2stat = Element.pm2_env.status //online , offline, stopped
+                result = makepretty(filetree, pm2stat);
+                //console.log(result);
+                sendArray.push(result);
+                sendtomain = sendArray
+                sendtomaster(sendArray)
+            });
+        };
     });
-    return result;
-    }
-    catch (error ){
-        console.error(error)
-        return error
-    }
 }
 
-function makepretty(filename, filetree, pm2stat){
+function makepretty(filetree, pm2stat){
     const cutArray = filetree.split('\\');
     //console.log(cutArray); //gives array split between folders
     totalLen = cutArray.length
@@ -62,3 +50,13 @@ function makepretty(filename, filetree, pm2stat){
     results = getThat +":"+ pm2stat
     return results
 }
+
+function sendtomaster(data){
+    process.send({ //this is just example, boiletplate for future apps
+      type : 'process:msg',
+      data : {
+        app : filename,
+        msg : data
+      }
+    })
+  }
