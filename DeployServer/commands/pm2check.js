4855bfd7644deae6c7f21d3b99c7ba84c3bbabe0
BlueThinks <bluekitt4u@gmail.com>
2025-03-19 14:35:40 +0200

    pm2check half works

--- DeployServer/commands/pm2check.js
+++ DeployServer/commands/pm2check.js
@@ -12,9 +12,9 @@ module.exports = {
     data: pm2check()
 };
 
-function pm2check(){
+async function pm2check(){
     //console.log("pm2check");
-    pm2.list((err, list) => {
+    sendtomaster = pm2.list((err, list) => {
         const id = 0;
         list = list.map(item => {
             return item.id !== id ? item : null;
@@ -25,20 +25,25 @@ function pm2check(){
         //they just served as check for other command, but now they arent needed, but they functionality is useful
         if(err != null){
             console.log(err)
+            return err
         }
         else {
             list.forEach((Element) => {
                 filetree = Element.pm2_env.pm_exec_path // /DeployServer/Depositories/
                 pm2stat = Element.pm2_env.status //online , offline, stopped
                 result = makepretty(filetree, pm2stat);
-                //console.log(result);
+                console.log(result);
                 sendArray.push(result);
                 sendtomain = sendArray
                 sendtomaster(sendArray)
+                return sendArray
             });
+            console.log("3")
         };
+        console.log("2")
+        return sendtomaster
     });
-}
+    console.log("1")}
 
 function makepretty(filetree, pm2stat){
     const cutArray = filetree.split('\\');
