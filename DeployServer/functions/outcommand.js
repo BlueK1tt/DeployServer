56a0212515e0b076a4b39867f6c6aab56ae945ce
BlueThinks <bluekitt4u@gmail.com>
2025-04-05 21:21:35 +0300

    testing works of outcommand

--- DeployServer/functions/outcommand.js
+++ DeployServer/functions/outcommand.js
@@ -5,11 +5,11 @@ const config = require("../resources/config.json");
 //need to connect to github and download same named repository from there
 
 module.exports = {
-    data : filtercommand(msg)
+    data : filtercommand(appdata)
 };
 
-function filtercommand(msg){
+function filtercommand(appdata){ 
     console.log("filtercommand")
-    
+    console.log("appdata " + appdata); //appdata gives server + the button pressed | Ticker : Test 
     return "end."
 }
