2e331204af94fb90733efb068c7e2c1fe21c235e
BlueThinks <bluekitt4u@gmail.com>
2025-05-12 13:21:05 +0300

    odd error with info outisde commands

--- DeployServer/Depositories/BluBot/commands/admin/userban.js
+++ DeployServer/Depositories/BluBot/commands/admin/userban.js
@@ -4,7 +4,18 @@ module.exports = {
     data: hello(info)
 };
 
-function hello(){
-    //console.log(msg)
+function hello(info){
+    console.log(typeof(info))
+    cutmessage(info)
 	return "User banned"
+}
+function cutmessage(info){
+    //function to cut the message received to be only command part IE banuser instead of !banuser Jorma
+    mesg = info.message
+    message = mesg.split("!")
+
+    message = msg[1].toString()
+    command = message.split(" ")
+    target = command[1]
+    console.log(target)
 }
