1b00206ab1e6fa06d2db41c378306c661be360b2
BlueThinks <bluekitt4u@gmail.com>
2025-05-13 11:25:56 +0300

    Two new admin commands, needed, but currently just the boilerplate

---
+++ DeployServer/Depositories/BluBot/commands/admin/userkick.js
@@ -0,0 +1,21 @@
+const { info } = require('../../server');
+
+module.exports = {
+    data: hello(info)
+};
+
+function hello(info){
+    console.log(typeof(info))
+    cutmessage(info)
+	return "User kicked"
+}
+function cutmessage(info){
+    //function to cut the message received to be only command part IE banuser instead of !banuser Jorma
+    mesg = info.message
+    message = mesg.split("!")
+
+    message = msg[1].toString()
+    command = message.split(" ")
+    target = command[1]
+    console.log(target)
+}
