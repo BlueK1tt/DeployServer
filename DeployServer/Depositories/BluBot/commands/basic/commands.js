91af2351867440ee88aaef66b5b3cc0dbcc99b48
BlueThinks <bluekitt4u@gmail.com>
2025-05-15 11:52:59 +0300

    added '!commands' command, gives commands based on if user is staff or not

---
+++ DeployServer/Depositories/BluBot/commands/basic/commands.js
@@ -0,0 +1,37 @@
+const Discord = require('discord.js');
+const { info, roles } = require('../../server');
+var path = require('path');
+const fs = require('fs'); //filesystem
+
+const admin = path.join(__dirname, '../admin/');
+const basic = path.join(__dirname, '../basic/')
+
+module.exports = {
+    data: getcommands(info)
+};
+
+function getcommands(info){
+	console.log("commands")
+    const commandsbasic = fs.readdirSync(basic); //get all files in basic commands folder
+    const commandsadmin = fs.readdirSync(admin); //get all files in admin commands folder
+    fs.close
+
+    //cleanup
+    admincommands = commandsadmin.map(function(d) {
+        return d.replace('.js', "");
+    });
+    var giveadmin = admincommands.toString();
+
+    basiccommands = commandsbasic.map(function(d) {
+        return d.replace('.js', "");
+    });
+    var givebasic = basiccommands.toString();
+
+    if(info.info.isStaff === true){
+        var givecommands = givebasic +","+ giveadmin
+        return givecommands 
+    }
+    else{
+        return givebasic
+    } 
+}
