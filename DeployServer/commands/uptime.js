be4af90773bf5a9fade7c2ebd266117a0d0689fd
BlueK1tt <Bluekitt4u@gmail.com>
2025-10-30 14:38:39 +0200

    Uptime command done and working.

--- DeployServer/commands/uptime.js
+++ DeployServer/commands/uptime.js
@@ -1,21 +1,15 @@
 //command to check how long server has been up
-const { diff } = require("util");
 let { timenow } = require("../server");
-//var oldreqtime = null; //just as start when there is not last request
-console
 
 module.exports =  {
     data: uptime()
-
 };
 
 function uptime() {
     let currenttime = new Date();
     let currenttimestr = JSON.stringify(currenttime)
-
     let oldtimestr = JSON.stringify(timenow)
-    //console.log(JSON.stringify(timenow)); // server start time
-    console.log(oldtimestr)
+
     let cutobject = oldtimestr.slice(11,-1)
     let oldobj = JSON.parse(cutobject)
     let cleannewtime = currenttimestr.slice(1,-6)
@@ -23,40 +17,33 @@ function uptime() {
     let oldsplit = oldobj.split("T") //0 = date, 1 = time
     var newsplit = cleannewtime.split("T")
     var oldcleantime = oldsplit[1].slice(0,-5)
-    console.log(oldcleantime)
-    console.log(newsplit[1])
-    
+
     let timeobj = timeobjects(oldcleantime, newsplit)
-    let timediff = difference(timeobj[0], timeobj[1])
-    console.log(timediff)
-    return "Server has been online for:";
+    return timeobj
 }
 
 function timeobjects(oldcleantime, newsplit){
-    console.log("timeobject")
     //oldtime and newtime strings cut and split into object
     let newtimesplit = newsplit[1].split(":")
     let newsection = new Object
     newsection['hours'] = newtimesplit[0]
     newsection['minutes'] = newtimesplit[1]
     newsection['seconds'] = newtimesplit[2]
-    //console.log(newsection)
 
     let oldtimesplit = oldcleantime.split(":")
     let oldsection = new Object
     oldsection['hours'] = oldtimesplit[0]
     oldsection['minutes'] = oldtimesplit[1]
     oldsection['seconds'] = oldtimesplit[2]
-    //console.log(oldsection)
 
+    let diffhour = newsection.hours - oldsection.hours
+    let diffmin = newsection.minutes - oldsection.minutes
+    let diffsec = newsection.seconds - oldsection.seconds
 
-    return [newsection, oldsection]; 
-}
-
-function difference(newsection, oldsection){
-    console.log("difference ")
-    //console.log(oldsection) //when the server was started
-    //console.log(newsection) //
-
-    return "Server has been online for:";
+    let returnhours = diffhour > 0 ? diffhour + (diffhour == 1 ? " hour, " : " hours, ") : "";
+    let returnmins = diffmin > 0 ? diffmin + (diffmin == 1 ? " minute, " : " minutes, ") : "";
+    let returnsecs = diffsec > 0 ? diffsec + (diffsec == 1 ? " second, " : " seconds, ") : "";
+    let returncombined = "Server has been online for,"+returnhours+returnmins+returnsecs
+    console.log(returnhours+returnmins+returnsecs)
+    return returncombined
 }
