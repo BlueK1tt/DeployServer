23ac22fbfbe2ca32e3f56a43aeaa2c69e0bcb7c1
BlueThinks <bluekitt4u@gmail.com>
2025-11-05 01:29:47 +0200

    disablecommand designing. writenew and if/else

--- DeployServer/commands/disablecommand.js
+++ DeployServer/commands/disablecommand.js
@@ -5,14 +5,35 @@ module.exports =  {
 };
 
 function disablecommand(){ //command to disable or enable command
-    console.log("disablecommand")
+    console.log("---disablecommand---")
     console.log(message)
+    let command = message.slice(10,-1) //need to slice the excess
+
     //array of all commands ['command','enabled'],etc...
 
     //disable is toggle, if enabled>disable, if disabled>enable
     //read ./commands folder and commands JSON, crossmatch
     //if some command isnt in JSON make object out of it, default enabled
     
+    if(commandexists === false){ //if command doesnt exist
+        writenew(command);
+        console.log("Making log of command"+$command+"...") //need to show that new log is being created
+        return
+    } else { //if command exists, disable or enable
+        
+        //if command is enabled, disable it
+
+        //if command is disabled, enable it
+
+    }
+
+    //need to read JSON file
+    return;
+}
+
+function writenew(){ 
+    console.log("---writenew---")
+
     var commandname = "command";
     var commandstatus = "enabled";
 
@@ -21,6 +42,4 @@ function disablecommand(){ //command to disable or enable command
     varcommand['name'] = commandname; //get command name
     varcommand['status'] = commandstatus; //enabled or disabled
 
-    //need to read JSON file
-    return;
 }
