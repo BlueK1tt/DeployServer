f2a92254780eda71715d992917977ae055140072
BlueThinks <bluekitt4u@gmail.com>
2026-01-14 16:00:38 +0200

    spacing

--- DeployServer/functions/logtemps.js
+++ DeployServer/functions/logtemps.js
@@ -37,29 +37,29 @@ function logtemps(){
         console.log(newstr)
         
         
-            
-            //to add commands used to 'temps.json' always add to the end with when used(server uptime)
-            //let tempdata = fs.readFileSync('./resources/temps.json', { encoding: 'utf8'});
-            //console.log("this is data:"+tempdata)
         
-        
-            //every time used, get "refreshed" log file, with latest command
-        
-        
-            //get used commands as array or string, split 
-        
-        
-            //if last 3 same commands within under 5 minutes are same disabled command, disable the log for it
-            
-        
-            //put the check at start of 'msgidentify', so check before identifying
-        
-        
-            passstatus = true
-            return passstatus;
-        }else {
-            console.log("else error")
-            return;
-        }
+        //to add commands used to 'temps.json' always add to the end with when used(server uptime)
+        //let tempdata = fs.readFileSync('./resources/temps.json', { encoding: 'utf8'});
+        //console.log("this is data:"+tempdata)
     
+    
+        //every time used, get "refreshed" log file, with latest command
+    
+    
+        //get used commands as array or string, split 
+    
+    
+        //if last 3 same commands within under 5 minutes are same disabled command, disable the log for it
+        
+    
+        //put the check at start of 'msgidentify', so check before identifying
+    
+    
+        passstatus = true
+        return passstatus;
+    }else {
+        console.log("else error")
+        return;
+    }
+
 }
